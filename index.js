7e1a58a87cc920016cc73a11841599ab0a8427a3
derekhe <derekhe@april1985.com>
2015-02-01 17:17:46 +0800

    增加verbose选项以减少打印

--- index.js
+++ index.js
@@ -3,21 +3,33 @@ var fs = require('fs'),
     request = require('request'),
     async = require('async'),
     keys = require('./keys'),
-    moment = require('moment');
+    moment = require('moment'),
+    argv = require('yargs').argv;
+
 
 var tasks = {};
 _.forEach(keys, function (n, key) {
     tasks[key] = function (callback) {
         request("http://mapidroid.aqicn.org/aqicn/json/android/" + n, function (err, response, body) {
             var data = JSON.parse(body);
-            console.log(key, data);
+            if (argv.verbose) {
+                console.log(key, data);
+            }
+            else{
+                console.log("Got", key);
+            }
             callback(err, data);
         })
     };
 });
 
 async.parallel(tasks, function (err, results) {
-    console.log("All results", results);
+    if (argv.verbose) {
+        console.log("All results", results);
+    }
+    else{
+        console.log("Results count:" + _.size(results));
+    }
     var path = "data/" + moment().format("YYYY_MM_DD_hh_mm_ss_ZZ") + ".json";
     fs.writeFileSync(path, JSON.stringify(results, null, 4));
 });
